956bfe9af37f1a54d6006ecdc5a469b719b8c2ed
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-05-21 18:17:24 +0900

    서버 연동시 Expected value at line 1 column 9 path $ 오류 발생

--- app/src/main/java/com/ihh/springtest/MainActivity.java
+++ app/src/main/java/com/ihh/springtest/MainActivity.java
@@ -1,13 +1,14 @@
 package com.ihh.springtest;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,44 +17,64 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText inputText;
-    private TextView outputText;
-    private Button sendBtn;
+
+    private static final String BASE_URL = "http://172.30.1.14:8080"; // Replace with your server URL
+    private static Retrofit retrofit;
+    private EditText editText;
+    private Button sendButton;
+    private TextView responseTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inputText = findViewById(R.id.input_text);
-        outputText = findViewById(R.id.output_text);
-        sendBtn = findViewById(R.id.btn_send);
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http:localhost:80")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        sendBtn.setOnClickListener(new View.OnClickListener() {
+
+        editText = findViewById(R.id.input_text);
+        sendButton = findViewById(R.id.btn_send);
+        responseTextView = findViewById(R.id.output_text);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                ApiService service = retrofit.create(ApiService.class);
-                Call<String> call = service.sendText(inputText.getText().toString());
-
-                call.enqueue(new Callback<String>() {
-                    @Override
-                    public void onResponse(Call<String> call, Response<String> response) {
-                        Log.d("inhohyun","sucess");
-                        outputText.setText(response.body());
-                    }
-
-                    @Override
-                    public void onFailure(Call<String> call, Throwable t) {
-                        Log.d("inhohyun2","fail");
-                        t.printStackTrace();
-
-                    }
-                });
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                sendTextToServer(text);
             }
         });
+    }
 
+    private void sendTextToServer(String text) {
 
+        if (retrofit == null){
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
 
+             retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+//        TextRequest textRequest = new TextRequest(text);
+
+        Call<String> call = apiService.sendText(text);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    String receivedText = response.body();
+                    responseTextView.setText(receivedText);
+                } else {
+                    responseTextView.setText("Request Failed1: " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                responseTextView.setText("Request Failed2: " + t.getMessage());
+            }
+        });
     }
 }
